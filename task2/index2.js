a73ab45afab0f9c9a2582a9d13b5e5c28d39a839
Iryna <irina_krupenina@ukr.net>
2023-11-09 10:17:34 +0200

    changes for task #2

--- task2/index2.js
+++ task2/index2.js
@@ -1,15 +1,21 @@
 let hText = document.querySelector('h1');
 hText.style.backgroundColor = 'green';
 
-let firstP = document.querySelector('div p:first-child');
-let secondP = document.querySelector('#myDiv p:nth-child(2)');
-let thirdP = document.querySelector('#myDiv p:nth-child(3)');
-let fourthP = document.querySelector('div p:last-child');
+const pText = document.querySelectorAll("#myDiv p");
+pText[0].style.fontWeight = 'bold';
+pText[1].style.color = 'red';
+pText[2].style.textDecoration = 'underline';
+pText[3].style.fontStyle = 'italic';
 
-firstP.style.fontWeight = 'bold';
-secondP.style.color = 'red';
-thirdP.style.textDecoration = 'underline';
-fourthP.style.fontStyle = 'italic';
+// let firstP = document.querySelector('div p:first-child');
+// let secondP = document.querySelector('#myDiv p:nth-child(2)');
+// let thirdP = document.querySelector('#myDiv p:nth-child(3)');
+// let fourthP = document.querySelector('div p:last-child');
+
+// firstP.style.fontWeight = 'bold';
+// secondP.style.color = 'red';
+// thirdP.style.textDecoration = 'underline';
+// fourthP.style.fontStyle = 'italic';
 
 let ulText = document.querySelectorAll('#myList li');
 let listText = '';
